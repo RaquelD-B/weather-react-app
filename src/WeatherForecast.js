54ea9bcda567d23f685c45e2b6dcdfadfea5aa29
RaquelD-B <154616671+RaquelD-B@users.noreply.github.com>
2024-03-16 15:58:35 -0300

    Update WeatherForecast.js

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -28,6 +28,8 @@ export default function WeatherForecast(props) {
                   <DailyForecast data={dailyforecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
